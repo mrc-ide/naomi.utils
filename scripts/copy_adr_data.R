3b00cc11263d551b4ab792755d3e3c4a8996dd97
Robert Ashton <r.ashton@imperial.ac.uk>
2021-11-29 14:47:35 +0000

    Create package in country org

--- scripts/copy_adr_data.R
+++ scripts/copy_adr_data.R
@@ -103,8 +103,9 @@ for (package in packages_copy) {
   message("Creating package for ", package[["geo-location"]])
   if (!dry_run) {
     tryCatch({
+      new_package <- NULL
       new_package <- ckanr::package_create(
-        type = dest, owner_org = "imperial-college-london",
+        type = dest, owner_org = package[["organization"]][["name"]],
         extras = list("geo-location" = package[["geo-location"]],
                       type_name = dest_name,
                       maintainer_email = "naomi-support@unaids.org",
