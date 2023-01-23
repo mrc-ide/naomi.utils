61d89b0185e24f9f57057765a4b44264d813fe6e
osymandius <bolistevens@gmail.com>
2023-01-23 12:25:15 +0000

    Remove system_file

--- R/utils_age.R
+++ R/utils_age.R
@@ -5,7 +5,7 @@
 #' @return data frame
 #' @export
 get_age_groups <- function() {
-  groups <- readr::read_csv(system_file("metadata", "meta_age_group.csv"))
+  groups <- readr::read_csv(system.file("metadata", "meta_age_group.csv", package = "naomi.utils", mustWork = TRUE))
   groups$age_group_span <- as.numeric(groups$age_group_span)
   groups
 }
